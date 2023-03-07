2c793956fc5f0f378d93c4f85cb8417c195f7eb2
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-07 16:36:34 -0500

    Add CORS library

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+// const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
 // app.use(express.urlencoded({extended: false}));
 // app.use(express.json());
+// app.user(cors());
 
 app.get('/', function (req, res) {
   res.send('Hello World');
